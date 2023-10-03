3e3d168a08d20cf8124a9cfae6b4f3c914d1a82b
Carlos Hardman <113129320+Carlos-Hardman@users.noreply.github.com>
2023-10-02 22:38:19 -0300

    Add files via upload

---
+++ NetBeansProjects/Pessoa/src/Pessoa.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carlos.ahjunior
+ */
+public class Pessoa {
+   private String Nome;
+     private int Idade;
+
+    public int setIdade(int Idade) {
+       return  this.Idade = Idade;
+    }
+    
+      public String setNome(String Nome) {
+       return  this.Nome = Nome;
+    }
+
+   
+    
+   
+    String getNome(){
+        
+            return Nome;
+        }
+    
+    int getIdade(){
+      
+            return Idade;
+        }
+    
+   
+
+
+}
+
---
+++ NetBeansProjects/Pessoa/src/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carlos.ahjunior
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Pessoa dados = new Pessoa();
+              
+        dados.setNome("Carlos");
+        dados.setIdade(20);
+
+        System.out.println(dados.getNome());
+         System.out.println(dados.getIdade());
+
+        
+    }
+}
