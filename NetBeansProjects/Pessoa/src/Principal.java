ddfcebd558dd4c1fe14052eb06bd0bd68c2c10cb
Carlos Hardman <113129320+Carlos-Hardman@users.noreply.github.com>
2023-10-04 11:42:08 -0300

    Update Principal.java

--- NetBeansProjects/Pessoa/src/Principal.java
+++ NetBeansProjects/Pessoa/src/Principal.java
@@ -8,15 +8,22 @@
  * @author carlos.ahjunior
  */
 public class Principal {
-    public static void main(String[] args) {
-        Pessoa dados = new Pessoa();
-              
-        dados.setNome("Carlos");
-        dados.setIdade(20);
+  public static void main(String[] args) {
+    Pessoa meusDados = new Pessoa();
 
-        System.out.println(dados.getNome());
-         System.out.println(dados.getIdade());
+    meusDados.setNome("Carlos");
+    meusDados.setIdade(20);
+    meusDados.setEstadoCivil("Solteiro");
+    meusDados.setData("19/02/2003");
+    meusDados.setCPF("871.969.680-93");
+    meusDados.setRG("121908288/99");
+    
 
-        
-    }
+    System.out.println("Nome: " + meusDados.getNome());
+    System.out.println("Idade: " + meusDados.getIdade() + " Anos ");
+    System.out.println("Estados " + meusDados.getEstadoCivil());
+    System.out.println("Data de Nascimento: " + meusDados.getData());
+    System.out.println("CPF: " + meusDados.getCPF());
+    System.out.println("RG: " + meusDados.getRG());
+  }  
 }
