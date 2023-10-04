2399897ee8cfb255bc902d55dc3da358aca62ebc
Carlos Hardman <113129320+Carlos-Hardman@users.noreply.github.com>
2023-10-04 11:41:43 -0300

    Update Pessoa.java

--- NetBeansProjects/Pessoa/src/Pessoa.java
+++ NetBeansProjects/Pessoa/src/Pessoa.java
@@ -7,33 +7,60 @@
  *
  * @author carlos.ahjunior
  */
-public class Pessoa {
-   private String Nome;
-     private int Idade;
+public class Pessoa{
+    private String Nome;
+    private String EstadoCivil;
+    private String Data;
+    private String CPF;
+    private String RG;
+    private int Idade;
 
-    public int setIdade(int Idade) {
-       return  this.Idade = Idade;
-    }
-    
-      public String setNome(String Nome) {
-       return  this.Nome = Nome;
-    }
-
-   
-    
-   
     String getNome(){
-        
-            return Nome;
-        }
-    
-    int getIdade(){
-      
-            return Idade;
-        }
-    
-   
+        return Nome;
+    }
 
+    int getIdade(){
+        return Idade;
+    }
+
+    String getEstadoCivil(){
+        return EstadoCivil;
+    }
+
+    String getData(){
+        return Data;
+    }
+
+    String getCPF(){
+        return CPF;
+    }
+
+    String getRG(){
+        return RG;
+    }
+
+    public String setNome(String Nome){
+        return this.Nome = Nome;
+    }
+
+    public int setIdade(int Idade){
+        return this.Idade = Idade;
+    }
+
+    public String setEstadoCivil(String EstadoCivil){
+        return this.EstadoCivil = EstadoCivil;
+    }
+
+    public String setData(String Data){
+        return this.Data = Data;
+    }
+    public String setCPF(String CPF){
+        return this.CPF = CPF;
+    }
+
+    public String setRG(String RG){
+        return this.RG = RG;
+    }
 
 }
 
